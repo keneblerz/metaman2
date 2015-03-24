c1078a288dae65b73a566878d5417d9309e09740
felix <frolle6@gmail.com>
2015-03-23 18:14:10 -0600

    Revert "Fixing Physics Step 1"
    This reverts commit 858128d74f764691ccd9704c2956b4103c384037.

--- core/src/com/mygdx/game/Player.java
+++ core/src/com/mygdx/game/Player.java
@@ -33,21 +33,16 @@ public class Player extends EntActor {
         FixtureDef fixtureDef =new FixtureDef();
         fixtureDef.shape = circle;
         fixtureDef.density = 1f;
-        fixtureDef.friction = .5f; //no friction against other surfaces //should be between 0 and 1
-        fixtureDef.restitution = .1f; // 0 = no bounce, 1 = perfect bounce
-
+        fixtureDef.friction = 0f; //no friction against other surfaces
+        fixtureDef.restitution = 0; // Make it not bounce
 
         BodyDef bodyDef = new BodyDef();
         // We set our body to dynamic, for something like ground which doesn't move we would set it to StaticBody
         bodyDef.type = BodyDef.BodyType.DynamicBody;
-        bodyDef.position.set(50, 120);
-
+        bodyDef.position.set(50, Game.cam.viewportHeight);
         Body body = Game.world.createBody(bodyDef);
         f = body.createFixture(fixtureDef);
 
-
-        body.setGravityScale(14f); //gravityScale = 0 means this thing will just float, 1.0 is default, use this to change how fast THIS object falls
-        body.setFixedRotation(true); //this body NEVER rotates despite torque/inertia applied to it
 
         setState(State.IDLE);
 
@@ -64,7 +59,7 @@ public class Player extends EntActor {
         if(Gdx.input.isKeyPressed(Input.Keys.W)){
             if(state!= State.JUMPING){
                 setState(State.JUMPING);
-//                System.out.println(stateTime);
+                System.out.println(stateTime);
 
             }
             if(animation.isAnimationFinished(stateTime)){ //we need it to stop at frame .4
@@ -72,7 +67,7 @@ public class Player extends EntActor {
                 updateState = false;
             }
             //f.getBody().applyLinearImpulse(0.0f, -10000.0f, f.getBody().getPosition().x, f.getBody().getPosition().y, true);
-            f.getBody().setLinearVelocity(f.getBody().getLinearVelocity().x,1000);
+            f.getBody().setLinearVelocity(f.getBody().getLinearVelocity().x,1500);
         }
 
         if(Gdx.input.isKeyPressed(Input.Keys.S)){
@@ -82,16 +77,15 @@ public class Player extends EntActor {
         if(Gdx.input.isKeyPressed(Input.Keys.A) ){
             if(state!= State.RUNNING && state!= State.JUMPING) setState(State.RUNNING);
             reverse = true;
-            f.getBody().applyLinearImpulse(-5000.0f, 0.0f,f.getBody().getPosition().x,f.getBody().getPosition().y, true);
-            //f.getBody().applyForce(-100000.0f, 0.0f, f.getBody().getPosition().x, 0, true );
+            //f.getBody().applyLinearImpulse(-2000.0f, 0.0f,f.getBody().getPosition().x,f.getBody().getPosition().y, true);
+            f.getBody().applyForce(-100000.0f, 0.0f, f.getBody().getPosition().x, 0, true );
         }
 
         if(Gdx.input.isKeyPressed(Input.Keys.D) ){
             if(state!= State.RUNNING && state!= State.JUMPING) setState(State.RUNNING);
             reverse = false;
-            f.getBody().applyLinearImpulse(5000f, 0.0f,f.getBody().getPosition().x,f.getBody().getPosition().y, true);
-            //f.getBody().setLinearVelocity(200f,0);
-            //f.getBody().applyForce(100000.0f, 0.0f, f.getBody().getPosition().x, 0, true );
+            //f.getBody().applyLinearImpulse(2000f, 0.0f,f.getBody().getPosition().x,0, true);
+            f.getBody().applyForce(100000.0f, 0.0f, f.getBody().getPosition().x, 0, true );
         }
 
         if (!Gdx.input.isKeyPressed(Input.Keys.A) && !Gdx.input.isKeyPressed(Input.Keys.D) && f.getBody().getLinearVelocity().y == 0){
